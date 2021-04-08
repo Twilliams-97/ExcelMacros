591a7a88a10ae580dce2a5074df36c5730333068
Twilliams-97 <73836410+Twilliams-97@users.noreply.github.com>
2021-04-08 14:05:49 +0100

    Create Selected Entire Row if Blank.vb
    Selects the entire row if the cell in that row in a selection is blank

---
+++ Selected Entire Row if Blank.vb
@@ -0,0 +1,40 @@
+Sub Select_Entire_Blank_Rows()
+    'Select all entire blank rows in selected range
+    
+    Dim rRow As Range
+    Dim rSelect As Range
+    Dim rSelection As Range
+     
+      'Check that a range is selected
+      If TypeName(Selection) <> "Range" Then
+        MsgBox "Please select a range first.", vbOKOnly, "Select Blank Rows Macro"
+        Exit Sub
+      End If
+      
+      'Check that multiple cells are selected
+      If Selection.Cells.Count = 1 Then
+        Set rSelection = ActiveSheet.UsedRange
+      Else
+        Set rSelection = Selection
+      End If
+     
+      'Loop through each row and add blank rows to rSelect range
+      For Each rRow In rSelection.Rows
+        If WorksheetFunction.CountA(rRow) = 0 Then
+          If rSelect Is Nothing Then
+            Set rSelect = rRow
+          Else
+            Set rSelect = Union(rSelect, rRow)
+          End If
+        End If
+      Next rRow
+      
+      'Select blank rows
+      If rSelect Is Nothing Then
+        MsgBox "No blank rows were found.", vbOKOnly, "Select Blank Rows Macro"
+        Exit Sub
+      Else
+         rSelect.EntireRow.Select
+      End If
+  
+End Sub
