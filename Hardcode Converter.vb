cbe2567aa3e1a1505df7ef876ac5506fbe328d7b
Twilliams-97 <73836410+Twilliams-97@users.noreply.github.com>
2021-04-08 11:35:40 +0100

    Create Hardcode Converter.vb
    Converts the selected range to hardcode. E.g. all formulas will just become values.

---
+++ Hardcode Converter.vb
@@ -0,0 +1,11 @@
+Sub Hardcode_Converter()
+    'Created by Tom Williams
+    'This works on the selected range
+    With Selection
+    
+        'Hardcode the values
+        .Value = .Value
+    
+    End With
+
+End Sub
