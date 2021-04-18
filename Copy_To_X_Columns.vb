5df1cefdd1232d79a4ec3dd3f9f964ca6c7d2818
Twilliams-97 <73836410+Twilliams-97@users.noreply.github.com>
2021-04-18 22:02:41 +0100

    Update Copy_To_X_Columns.vb

--- Copy_To_X_Columns.vb
+++ Copy_To_X_Columns.vb
@@ -1,53 +1,43 @@
-Function SingleCheck(ByVal Target As Range)
+Function Redoer(ByVal textinput As String, ByRef Target As Range)
 
-    If Target.Count > 1 Then
-
-        MsgBox "Only one cell allowed to be selected at a time.", , "Single Cells Only"
-        SingleCheck = 1
-    End If
+        Do While Not Valid
+        
+        Set newtarget = Application.InputBox(textinput, "Turn 1 Column to X", Target.Address, Type:=8)
+        If newtarget.Count > 1 Then
+            MsgBox "Only select one cell. Please try again"
+            Valid = False
+        Else
+            Valid = True
+        End If
+    Loop
     
+    newtarget.Select
 
 End Function
 
-
-Sub Copy_to_X_Columns()
-
-    Dim valperrow As Integer
+Sub Copy_to_seXy_Columns()
+    
     Dim outputrng As Range
     Dim inputrng As Range
-    Dim bottomrow As Range
-    
-    xTitleId = "Turn 1 Column to X"
+    Dim bottomrow As Range    
 
     'how many values to put per row
-    
-    valperrow = Application.InputBox("How many columns? :", xTitleId, Type:=1)
+    Dim valperrow As Integer
+    valperrow = Application.InputBox("How many columns? :", xTitleId, , Type:=1)
     interval = valperrow
     
     'First cell in the column
     
     Set inputrng = Application.Selection
-    Set inputrng = Application.InputBox("Select First Cell in Column :", xTitleId, inputrng.Address, Type:=8)
-    
-    If SingleCheck(inputrng) = 1 Then
-        Exit Sub
-    End If
-   
-    inputrng.Select
+    firstcell = Redoer("Select First Cell in Column :", inputrng)
    
     first_row = ActiveCell.Row
-    first_col = ActiveCell.Column
-    
+    first_col = ActiveCell.Column 
     
     'Get the last cell in the column
     Set bottomrow = Application.Selection
-    Set bottomrow = Application.InputBox("Select Last Row in Column:", xTitleId, bottomrow.Address, Type:=8)
+    lastcell = Redoer("Select Last Row in Column:", bottomrow)
     
-    If SingleCheck(bottomrow) = 1 Then
-        Exit Sub
-    End If
-    
-    bottomrow.Select
     last_row = ActiveCell.Row
     
     'Uncomment the following (and comment above) if you want it to go all the way down the column
@@ -56,25 +46,17 @@ Sub Copy_to_X_Columns()
     
     'first cell where you want the data to go
     Set outputrng = Application.Selection
-    Set outputrng = Application.InputBox("Select top left of where you want it to go :", xTitleId, outputrng.Address, Type:=8)
+    outputcell = Redoer("Select top left of where you want it to go :", outputrng)
     
-    If SingleCheck(outputrng) = 1 Then
-        Exit Sub
-    End If
-    
-    outputrng.Select
-    
-    dest_start_col = ActiveCell.Column '3
-    dest_start_row = ActiveCell.Row '1
+    dest_start_col = ActiveCell.Column
+    dest_start_row = ActiveCell.Row
 
     dest_cur_col = dest_start_col
     dest_cur_row = dest_start_row
     
-
     For cur_row = first_row To last_row
   
         Cells(dest_cur_row, dest_cur_col).Value = Cells(cur_row, first_col).Value
-
         dest_cur_col = dest_cur_col + 1
     
         If (cur_row - (first_row - 1)) Mod interval = 0 Then
@@ -87,4 +69,3 @@ Sub Copy_to_X_Columns()
     Next
 
 End Sub
-
